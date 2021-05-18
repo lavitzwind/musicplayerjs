e6692fa4c06242365ae4a3e2847ee0bf6c537bc6
milleseseau <leojimlavi@gmail.com>
2021-05-18 03:59:18 -0500

    js

---
+++ script.js
@@ -0,0 +1,48 @@
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+const cover = document.querySelector('#cover');
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of songs
+let songIndex = 2;
+
+// Update song details
+const loadSong = (song) => {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+};
+
+const playSong = () => {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+};
+
+const pauseSong = () => {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+};
+
+// Initially load son info DOM
+loadSong(songs[songIndex]);
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+
+    if(isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
