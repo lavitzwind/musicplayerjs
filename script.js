4b7a16e9285a0fa94b31ffb8f99bccd3dc6822da
milleseseau <leojimlavi@gmail.com>
2021-05-18 05:04:26 -0500

    end project

--- script.js
+++ script.js
@@ -25,13 +25,56 @@ const playSong = () => {
     musicContainer.classList.add('play');
     playBtn.querySelector('i.fas').classList.remove('fa-play');
     playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+    audio.play()
 };
 
 const pauseSong = () => {
     musicContainer.classList.remove('play');
     playBtn.querySelector('i.fas').classList.add('fa-play');
     playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+    audio.pause();
 };
+
+const prevSong = () => {
+    songIndex--
+
+    if(songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+const nextSong = () => {
+    songIndex++
+
+    if(songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+const updateProgress = (e) => {
+    const {duration, currentTime} = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e){
+    const width = this.clientWidth;
+    console.log(width);
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+}
 
 // Initially load son info DOM
 loadSong(songs[songIndex]);
@@ -46,3 +89,13 @@ playBtn.addEventListener('click', () => {
         playSong();
     }
 });
+
+// Change song events
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+audio.addEventListener('timeupdate', updateProgress);
+
+progressContainer.addEventListener('click', setProgress);
+
+audio.addEventListener('ended', nextSong);
